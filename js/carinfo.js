380995da37a7f209cf58a942df44fc1cc44e8d49
faxhunmatcha001 <104335197+faxhunmatcha001@users.noreply.github.com>
2022-10-31 13:28:38 +0700

    Update carinfo.js

--- js/carinfo.js
+++ js/carinfo.js
@@ -3,7 +3,7 @@ $(".modal.animate__animated").on('shown.bs.modal', function () {
         $(this).removeClass('animate__fadeOut fn-modal-out');
         $(this).addClass('animate__fadeIn fn-modal-in');
         $(".modal-backdrop.fn-modal-fade").addClass("show");
-        if($(this).find("modal-dialog modal-content").hasClass("modal-fullscreen-lg-down")){
+        if($(this).find(".modal-dialog .modal-content").hasClass("modal-fullscreen-lg-down")){
             $(".modal-backdrop.fn-modal-fade").addClass("fn-modal-fullscreen");
         }
     }
